343bb06c3771925a0f70bdeeab7585aa7d0b85bf
michaelyu77777 <69225666+michaelyu77777@users.noreply.github.com>
2021-06-01 11:56:25 +0800

    統整code

--- appUpdateApkDownload/main.go
+++ appUpdateApkDownload/main.go
@@ -6,14 +6,17 @@ import (
 )
 
 const (
-	port = "5005"
+	port      = "5005"
+	basicPath = "/appUpdate/download/"
 
+	//URL路徑
 	pathName1 = "1" // 教育訓練語音Service
 	pathName2 = "2" // 教育訓練
 	pathName3 = "3" // 熱像偵測
 	pathName4 = "4" // EnvironmentSysteem
 	pathName5 = "5" // 教育訓練語音Service
 
+	// 檔名
 	fileName1 = "" // 教育訓練語音Service
 	fileName2 = "" // 教育訓練
 	fileName3 = "" // 熱像偵測
@@ -24,11 +27,11 @@ const (
 func main() {
 
 	// 檔案一下載路徑
-	http.HandleFunc("/appUpdate/download/"+pathName1, downloadFile1)
-	http.HandleFunc("/appUpdate/download/"+pathName2, downloadFile2)
-	http.HandleFunc("/appUpdate/download/"+pathName3, downloadFile3)
-	http.HandleFunc("/appUpdate/download/"+pathName4, downloadFile4)
-	http.HandleFunc("/appUpdate/download/"+pathName5, downloadFile5)
+	http.HandleFunc(basicPath+pathName1, downloadFile1)
+	http.HandleFunc(basicPath+pathName2, downloadFile2)
+	http.HandleFunc(basicPath+pathName3, downloadFile3)
+	http.HandleFunc(basicPath+pathName4, downloadFile4)
+	http.HandleFunc(basicPath+pathName5, downloadFile5)
 
 	// 開啟Port
 	http.ListenAndServe(":"+port, nil)
